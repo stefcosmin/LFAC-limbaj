9b43da7edb044eb0cf6dc570afbfbe99c8094485
andrei ciobanu <angryfk13@gmail.com>
2025-12-17 05:03:35 -0500

    bug: Fixed previous .bat and .sh scripts

--- compile.bat
+++ compile.bat
@@ -3,6 +3,6 @@ del lex.yy.c
 del %1.tab.c
 del %1.tab.h
 del %1.exe
-bison -d %1.y
-flex %1.l
-g++ lex.yy.c  %1.tab.c -o %1
+bison -d src/%1.y
+flex src/%1.l
+g++ -std=c++17 lex.yy.c  %1.tab.c -o %1
--- compile.sh
+++ compile.sh
@@ -6,6 +6,6 @@ echo "compiling $FILE"
 rm -f lex.yy.c
 rm -f $FILE.tab.c
 rm -f $FILE
-bison -d $FILE.y -Wcounterexamples
-lex $FILE.l
+bison -d src/$FILE.y -Wcounterexamples
+lex src/$FILE.l
 g++ lex.yy.c  $FILE.tab.c -o $FILE

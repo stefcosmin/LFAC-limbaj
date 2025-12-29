49bb73c695b60a4ff7e9f4a066482d4b42d0189c
stefcosmin <cosmin.stefanache2005@outlook.com>
2025-12-29 18:01:23 +0200

    fix yylineno error

--- compile.bat
+++ compile.bat
@@ -1,8 +1,8 @@
 echo "compiling %1"
-del 'src/lex.yy.c'
-del 'src/%1.tab.c'
-del 'src/%1.tab.h'
-del 'src/%1.exe'
+del 'lex.yy.c'
+del '%1.tab.c'
+del '%1.tab.h'
+del '%1.exe'
 bison -d src/%1.y
 flex src/%1.l
-g++  -std=c++11 lex.yy.c  %1.tab.c -o %1
+g++  -std=c++17 lex.yy.c  %1.tab.c -o %1
